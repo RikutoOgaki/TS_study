2dbef4967771d756ec8636a94e137606f8426469
Rikuto <orikutobasuke@gmail.com>
2022-12-21 17:22:11 +0900

    React typescript add

--- set/src/main.tsx
+++ set/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import Type from './type'
+// import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Type/>
   </React.StrictMode>,
 )
--- set/src/type.tsx
+++ set/src/type.tsx
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 
 function type() {
     const [text,setText] = useState();
+    let tex:string = "2"
+    let num:number = 4
+    
   return (
     <div>
         <input value={text} />
-        <h1></h1>
+        <h1>{num}</h1>
+        <h2>{tex}</h2>
     </div>
   )
 }
