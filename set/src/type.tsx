49095867b1c9e70805c459cd0e1fb39f38055159
Rikuto <orikutobasuke@gmail.com>
2022-12-22 22:43:11 +0900

    React typescript add

--- set/src/type.tsx
+++ set/src/type.tsx
@@ -4,12 +4,17 @@ function type() {
     const [text,setText] = useState();
     let tex:string = "2"
     let num:number = 4
+
+    let some:any;
+    some = 123;  
+    some = "はるや";
     
   return (
     <div>
         <input value={text} />
         <h1>{num}</h1>
         <h2>{tex}</h2>
+        <h3>{some}</h3>
     </div>
   )
 }
