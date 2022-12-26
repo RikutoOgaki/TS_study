48118cef83e5b4c9df428e36bc758179569e4a39
Rikuto <orikutobasuke@gmail.com>
2022-12-26 14:54:10 +0900

    React typescript add

--- set/src/type.tsx
+++ set/src/type.tsx
@@ -2,15 +2,26 @@ import React, { useState } from 'react'
 
 function type() {
     const [text,setText] = useState();
+
+    // 基本的な型定義
     let tex:string = "2"
     let num:number = 4
 
+    // any型
     let some:any;
     some = 123;  
     some = "はるや";
 
-    type mosu = Number | string;
-    mosu = 123
+    // インターフェイス
+    interface User{
+      name:string;
+      full_name:string;
+    }
+    let name:User;
+    name = {
+      name:"りくと",
+      full_name:"大垣陸斗"
+    }
     
   return (
     <div>
@@ -18,7 +29,6 @@ function type() {
         <h1>{num}</h1>
         <h2>{tex}</h2>
         <h3>{some}</h3>
-        <h4>{}</h4>
     </div>
   )
 }
