51c3b72b97c0eea71cdf4136b11ace0190f52644
Rikuto <orikutobasuke@gmail.com>
2023-01-08 07:00:36 +0900

    Typescript todo add

--- set/src/main.tsx
+++ set/src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 // import App from './App'
 import Type from './type'
 // import './index.css'
+import Todo from './todo'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     {/* <App /> */}
-    <Type/>
+    {/* <Type/> */}
+    <Todo/>
   </React.StrictMode>,
 )
---
+++ set/src/todo.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+function todo() {
+    const[todo,setTodo] = useState("");
+    const[add,setAdd] = useState("");
+
+
+    const clickEvent = () =>{
+        setAdd(todo)
+        setTodo("");
+    }
+    const clickDelete = () =>{
+      setAdd("");
+    }
+  return (
+    <div>
+            <input
+            value={todo}
+            onChange={(event)=>setTodo(event.target.value)}
+        />
+        <button onClick={clickEvent}>追加</button>
+        <button onClick={clickDelete}>削除</button>
+        <p>{add}</p>
+    </div>
+  )
+}
+
+export default todo
