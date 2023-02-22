4a63deaa7667552d69bd71fc863238b42cf3cbc7
Rikuto <orikutobasuke@gmail.com>
2023-02-22 18:10:20 +0900

    React typescript add

---
+++ set/src/sample02.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function sample02() {
+
+    const [state,setState] = useState();
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default sample02
